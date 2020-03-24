4f1c336032817de8aaa4d34b32face555edc921d
Ylannl <r.y.peters@tudelft.nl>
2020-03-24 18:59:54 +0100

    refactor eptinpolygons node

--- src/points_in_polygons.cpp
+++ src/points_in_polygons.cpp
@@ -31,6 +31,116 @@ pGridSet build_grid(const vec3f& ring) {
   return grid_set;
 }
 
+class PointsInPolygonsCollector  {
+  gfSingleFeatureInputTerminal& polygons;
+  gfSingleFeatureOutputTerminal& point_clouds;
+  gfSingleFeatureOutputTerminal& ground_elevations;
+
+  // ground elevations
+  std::vector<std::vector<float>> point_clouds_ground;
+  RasterTools::Raster pindex;
+  std::vector<std::vector<size_t>> pindex_vals;
+  std::vector<pGridSet> poly_grids;
+  
+  public:
+  Box completearea_bb;
+  float min_ground_elevation = std::numeric_limits<float>::max();
+
+  PointsInPolygonsCollector(gfSingleFeatureInputTerminal& polygons, gfSingleFeatureOutputTerminal& point_clouds, gfSingleFeatureOutputTerminal& ground_elevations, float& cellsize, float& buffer)
+  : polygons(polygons), point_clouds(point_clouds), ground_elevations(ground_elevations) {
+    point_clouds_ground.resize(polygons.size());
+
+    // make a vector of BOX2D for the set of input polygons
+    // build point in polygon grids
+    for (size_t i=0; i<polygons.size(); ++i) {
+      auto ring = polygons.get<LinearRing>(i);
+      poly_grids.push_back(build_grid(ring));
+      completearea_bb.add(ring.box());
+    }
+
+    // build an index grid for the polygons
+
+    // build raster index (pindex) and store for each raster cell all the polygons with intersecting bbox (pindex_vals)
+    float minx = completearea_bb.min()[0]-buffer; 
+    float miny = completearea_bb.min()[1]-buffer;
+    float maxx = completearea_bb.max()[0]+buffer;
+    float maxy = completearea_bb.max()[1]+buffer;
+    pindex =  RasterTools::Raster(cellsize, minx, maxx, miny, maxy);
+    pindex_vals.resize(pindex.dimx_*pindex.dimy_);
+
+    // populate pindex_vals
+    for (size_t i=0; i<polygons.size(); ++i) {
+      auto ring = polygons.get<LinearRing>(i);
+      auto& b = ring.box();
+      size_t r_min = pindex.getRow(b.min()[0], b.min()[1]);
+      size_t c_min = pindex.getCol(b.min()[0], b.min()[1]);
+      size_t r_max = static_cast<size_t>( ceil(b.size_y() / cellsize) ) + r_min;
+      size_t c_max = static_cast<size_t>( ceil(b.size_x() / cellsize) ) + c_min;
+      if (r_max >= pindex.dimy_)
+        r_max = pindex.dimy_-1;
+      if (c_max >= pindex.dimx_)
+        c_max = pindex.dimx_-1;
+      for(size_t r = r_min; r <= r_max; ++r) {
+        for(size_t c = c_min; c <= c_max; ++c) {
+          pindex_vals[r*pindex.dimx_ + c].push_back(i);
+        }
+      }
+    }
+
+  }
+
+  void add_point(arr3f point, int point_class) {
+    pPipoint pipoint = new Pipoint{point[0],point[1]};
+
+    // look up grid index cell and do pip for all polygons retreived from that cell
+    size_t lincoord = pindex.getLinearCoord(point[0],point[1]);
+    if (lincoord >= pindex_vals.size() || lincoord < 0) {
+      std::cout << "Point (" << point[0] << ", " <<point[1] << ", "  << point[2] << ") is not in the polygon bbox.\n";
+      return;
+    }
+    // For the ground points we only test if the point is within the grid
+    // index cell, but we do not do a pip for the footprint itself.
+    // The reasons for this:
+    //  - If we would do a pip for with the ground points, we would need to
+    //    have a separate list of buffered footprints that we use for the
+    //    ground pip. Still it would be often the case that there are no
+    //    ground points found for a buffered footprint. Thus we need a larger
+    //    area in which we can guarantee that we find at least a couple of
+    //    ground points.
+    //  - Because we work with Netherlands data, the ground relief is small.
+    //    Thus a single ground height value per grid cell is good enough for
+    //    representing the ground/floor elevation of the buildings in that
+    //    grid cell.
+    for(size_t& poly_i : pindex_vals[lincoord]) {
+      if (point_class == 2) {
+        point_clouds_ground[poly_i].push_back(point[2]);
+        min_ground_elevation = std::min(min_ground_elevation, point[2]);
+      } else if (point_class == 6) {
+        if (GridTest(poly_grids[poly_i], pipoint)) {
+          point_clouds.get<PointCollection&>(poly_i).push_back(point);
+        }
+      }
+    }
+  }
+
+  void compute_ground_elevation(float& ground_percentile) {
+    // Compute median ground height per grid cell and store it for each polygon
+    std::cout <<"Computing the median ground elevation per polygon..." << std::endl;
+    for (auto& z_vec : point_clouds_ground) {
+      // Median for odd-number of elements
+      float ground_ele = min_ground_elevation;
+      if (z_vec.size()!=0) {
+        ground_ele = compute_percentile(z_vec, ground_percentile);
+      } else {
+        std::cout << "no ground pts found for polygon\n";
+      }
+      // Assign the median ground elevation to each polygon
+      ground_elevations.push_back(ground_ele);
+    }
+  }
+};
+
+
 void LASInPolygonsNode::process() {
   auto& polygons = vector_input("polygons");
 
@@ -99,48 +209,7 @@ void EptInPolygonsNode::process()
   point_clouds.resize<PointCollection>(polygons.size());
   auto& ground_elevations = vector_output("ground_elevations");
 
-  // Intermediary storage for computing median ground heights
-  std::vector<std::vector<float>> point_clouds_ground;
-  point_clouds_ground.resize(polygons.size());
-
-  // make a vector of BOX2D for the set of input polygons
-  Box completearea_bb;
-  // build point in polygon grids
-  std::vector<pGridSet> poly_grids;
-  for (size_t i=0; i<polygons.size(); ++i) {
-    auto ring = polygons.get<LinearRing>(i);
-    poly_grids.push_back(build_grid(ring));
-    completearea_bb.add(ring.box());
-  }
-
-  // build an index grid for the polygons
-
-  // build raster index (pindex) and store for each raster cell all the polygons with intersecting bbox (pindex_vals)
-  float minx = completearea_bb.min()[0]-buffer; 
-  float miny = completearea_bb.min()[1]-buffer;
-  float maxx = completearea_bb.max()[0]+buffer;
-  float maxy = completearea_bb.max()[1]+buffer;
-  RasterTools::Raster pindex(cellsize, minx, maxx, miny, maxy);
-  std::vector<std::vector<size_t>> pindex_vals(pindex.dimx_*pindex.dimy_);
-
-  // populate pindex_vals
-  for (size_t i=0; i<polygons.size(); ++i) {
-    auto ring = polygons.get<LinearRing>(i);
-    auto& b = ring.box();
-    size_t r_min = pindex.getRow(b.min()[0], b.min()[1]);
-    size_t c_min = pindex.getCol(b.min()[0], b.min()[1]);
-    size_t r_max = static_cast<size_t>( ceil(b.size_y() / cellsize) ) + r_min;
-    size_t c_max = static_cast<size_t>( ceil(b.size_x() / cellsize) ) + c_min;
-    if (r_max >= pindex.dimy_)
-      r_max = pindex.dimy_-1;
-    if (c_max >= pindex.dimx_)
-      c_max = pindex.dimx_-1;
-    for(size_t r = r_min; r <= r_max; ++r) {
-      for(size_t c = c_min; c <= c_max; ++c) {
-        pindex_vals[r*pindex.dimx_ + c].push_back(i);
-      }
-    }
-  }
+  PointsInPolygonsCollector pip_collector{polygons, point_clouds, ground_elevations, cellsize, buffer};
 
   std::string ept_path = "ept://" + dirpath;
   {
@@ -158,11 +227,13 @@ void EptInPolygonsNode::process()
 
   // TODO: The reader and filter init can go to a function that returns a
   //  const PointViewSet.
+  auto pmin = pip_collector.completearea_bb.min();
+  auto pmax = pip_collector.completearea_bb.max();
   pdal::BOX2D poly_bbox(
-    minx + (*manager.data_offset)[0],
-    miny + (*manager.data_offset)[1],
-    maxx + (*manager.data_offset)[0],
-    maxy + (*manager.data_offset)[1]
+    pmin[0] - buffer + (*manager.data_offset)[0],
+    pmin[1] - buffer + (*manager.data_offset)[1],
+    pmax[0] + buffer + (*manager.data_offset)[0],
+    pmax[1] + buffer + (*manager.data_offset)[1]
   );
   pdal::EptReader reader;
   {
@@ -188,7 +259,6 @@ void EptInPolygonsNode::process()
   const pdal::PointViewSet pw_set(range.execute(ept_table));
 
   std::cout << "...PDAL Pipeline executed\n Continuing with point-in-polygon tests\n";
-  float min_ground_elevation = std::numeric_limits<float>::max();
 
   for (const pdal::PointViewPtr& view : pw_set) {
 
@@ -200,82 +270,12 @@ void EptInPolygonsNode::process()
       float py = view->getFieldAs<float>(pdal::Dimension::Id::Y, p) - (*manager.data_offset)[1];
       float pz = view->getFieldAs<float>(pdal::Dimension::Id::Z, p) - (*manager.data_offset)[2];
       int pclass = view->getFieldAs<int>(pdal::Dimension::Id::Classification, p);
-      pPipoint point = new Pipoint{px,py};
-
-      // look up grid index cell and do pip for all polygons retreived from that cell
-      size_t lincoord = pindex.getLinearCoord(px,py);
-      if (lincoord >= pindex_vals.size() || lincoord < 0) {
-        std::cout << "Point (" << px << ", " <<py << ", "  << pz << ") is not in the polygon bbox.\n";
-        continue;
-      }
-      // For the ground points we only test if the point is within the grid
-      // index cell, but we do not do a pip for the footprint itself.
-      // The reasons for this:
-      //  - If we would do a pip for with the ground points, we would need to
-      //    have a separate list of buffered footprints that we use for the
-      //    ground pip. Still it would be often the case that there are no
-      //    ground points found for a buffered footprint. Thus we need a larger
-      //    area in which we can guarantee that we find at least a couple of
-      //    ground points.
-      //  - Because we work with Netherlands data, the ground relief is small.
-      //    Thus a single ground height value per grid cell is good enough for
-      //    representing the ground/floor elevation of the buildings in that
-      //    grid cell.
-      bool found_pip(false);
-      for(size_t& poly_i : pindex_vals[lincoord]) {
-        if (pclass == 2) {
-          point_clouds_ground[poly_i].push_back(pz);
-          min_ground_elevation = std::min(min_ground_elevation, pz);
-        } else if (not found_pip and GridTest(poly_grids[poly_i], point)) {
-          point_clouds.get<PointCollection&>(poly_i).push_back({px, py, pz});
-          found_pip = true;
-        }
-      }
+      
+      pip_collector.add_point({px, py, pz}, pclass);
     }
   }
 
-  // DEBUG: write polygon WKT with ground height to file
-  // std::string fout("/tmp/geoflow_footprint_ground.tsv");
-  // std::ofstream out_tsv(fout.c_str());
-  // std::ostringstream wkt;
-  // wkt << "geom" << "\t" << "ground_height" << std::endl;
-  // // end DEBUG
-
-  // Compute median ground height per grid cell and store it for each polygon
-  std::cout <<"Computing the median ground elevation per polygon..." << std::endl;
-  for (auto& z_vec : point_clouds_ground) {
-    // Median for odd-number of elements
-    float ground_ele = min_ground_elevation;
-    if (z_vec.size()!=0) {
-      ground_ele = compute_percentile(z_vec, ground_percentile);
-    } else {
-      std::cout << "no ground pts found for polygon\n";
-    }
-    // Assign the median ground elevation to each polygon
-    ground_elevations.push_back(ground_ele);
-
-      // DEBUG: write polygon WKT with ground height to file
-      // auto polygon = polygons.get<LinearRing>(poly_i);
-      // wkt << std::fixed << std::setprecision(3) << "POLYGON((";
-      // for (auto& v : polygon) {
-      //   wkt << v[0] + (*manager.data_offset)[0] << " "
-      //       << v[1] + (*manager.data_offset)[1] << ", ";
-      // }
-      // wkt << polygon[0][0] + (*manager.data_offset)[0] << " "
-      //     << polygon[0][1] + (*manager.data_offset)[1];
-      // wkt << "))" << "\t";
-      // wkt << median << std::endl;
-      // end DEBUG
-  }
-  // // DEBUG: write polygon WKT with ground height to file
-  // if (out_tsv.is_open()) {
-  //   std::cout << "Writing " << fout << std::endl;
-  //   out_tsv << wkt.str();
-  // } else {
-  //   std::cout << "Could not open " << fout << std::endl;
-  // }
-  // out_tsv.close();
-  // // end DEBUG
+  pip_collector.compute_ground_elevation(ground_percentile);
 }
 
 }
