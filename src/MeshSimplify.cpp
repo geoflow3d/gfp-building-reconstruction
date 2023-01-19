074b08c400692a11dbcb579ecb790fa5e9c3887d
Ravi Peters <r.y.peters@tudelft.nl>
2023-01-19 23:02:07 +0100

    skip simplify if stop_ratio equal to 1

--- src/MeshSimplify.cpp
+++ src/MeshSimplify.cpp
@@ -37,34 +37,34 @@ namespace geoflow::nodes::stepedge {
                                     Border_is_constrained_edge_map > Placement;
 
   void MeshSimplifyNode::process() { 
-
     auto smesh = input("cgal_surface_mesh").get<SurfaceMesh>();
 
-    if(!CGAL::is_triangle_mesh(smesh)) CGAL::Polygon_mesh_processing::triangulate_faces(smesh);
-    
-    SurfaceMesh::Property_map<halfedge_descriptor, std::pair<K::Point_3, K::Point_3> > constrained_halfedges;
-    constrained_halfedges = smesh.add_property_map<halfedge_descriptor,std::pair<K::Point_3, K::Point_3> >("h:vertices").first;
-    std::size_t nb_border_edges=0;
-    for(halfedge_descriptor hd : halfedges(smesh))
-    {
-      if(CGAL::is_border(hd, smesh))
+    if (stop_ratio_ < 1.){
+      if(!CGAL::is_triangle_mesh(smesh)) CGAL::Polygon_mesh_processing::triangulate_faces(smesh);
+      
+      SurfaceMesh::Property_map<halfedge_descriptor, std::pair<K::Point_3, K::Point_3> > constrained_halfedges;
+      constrained_halfedges = smesh.add_property_map<halfedge_descriptor,std::pair<K::Point_3, K::Point_3> >("h:vertices").first;
+      std::size_t nb_border_edges=0;
+      for(halfedge_descriptor hd : halfedges(smesh))
       {
-        constrained_halfedges[hd] = std::make_pair(smesh.point(source(hd, smesh)),
-                                                  smesh.point(target(hd, smesh)));
-        ++nb_border_edges;
+        if(CGAL::is_border(hd, smesh))
+        {
+          constrained_halfedges[hd] = std::make_pair(smesh.point(source(hd, smesh)),
+                                                    smesh.point(target(hd, smesh)));
+          ++nb_border_edges;
+        }
       }
+      // Contract the surface mesh as much as possible
+      SMS::Count_ratio_stop_predicate<SurfaceMesh> stop(stop_ratio_);
+      Border_is_constrained_edge_map bem(smesh);
+      // This the actual call to the simplification algorithm.
+      // The surface mesh and stop conditions are mandatory arguments.
+      int r = SMS::edge_collapse(smesh, stop,
+                                CGAL::parameters::edge_is_constrained_map(bem)
+                                                  .get_placement(Placement(bem)));
+      // std::cout << "\nFinished!\n" << r << " edges removed.\n"
+                // << smesh.number_of_edges() << " final edges.\n";
     }
-    // Contract the surface mesh as much as possible
-    SMS::Count_ratio_stop_predicate<SurfaceMesh> stop(stop_ratio_);
-    Border_is_constrained_edge_map bem(smesh);
-    // This the actual call to the simplification algorithm.
-    // The surface mesh and stop conditions are mandatory arguments.
-    int r = SMS::edge_collapse(smesh, stop,
-                              CGAL::parameters::edge_is_constrained_map(bem)
-                                                .get_placement(Placement(bem)));
-    // std::cout << "\nFinished!\n" << r << " edges removed.\n"
-              // << smesh.number_of_edges() << " final edges.\n";
-    
     output("cgal_surface_mesh").set(smesh);
   }
 
